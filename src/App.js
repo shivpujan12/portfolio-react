c7919358e08e3bb1f77edb82c57f02a610d4ce31
Shivpujan yadav <31546372+shivpujan12@users.noreply.github.com>
2023-09-30 12:55:52 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 import Dashboard from "./components/Dashboard";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NoPage from "./components/NoPage";
 
 function App() {
   return (
-    <Dashboard/>
+      <>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Dashboard/>}/>
+              <Route path="/dashboard" element={<Dashboard/>}/>
+              <Route path="/resume" element={<Resume/>}/>
+              <Route path="/projects" element={<Projects/>}/>
+              <Route path="*" element={<NoPage/>}/>
+          </Routes>
+      </BrowserRouter>
+      </>
   );
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,12 @@ import '../styles/Dashboard.scoped.css';
 import SomeImage from '../assets/test2.jpeg';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub, faInstagram, faLinkedinIn, faStackOverflow, faTwitter} from "@fortawesome/free-brands-svg-icons";
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
+import {Route, Routes, useNavigate} from "react-router-dom";
+import Home from "./Home";
+import Resume from "./Resume";
+import NoPage from "./NoPage";
+import Projects from "./Projects";
 
 
 export default function Dashboard() {
@@ -11,34 +16,54 @@ export default function Dashboard() {
     const resumeRef = useRef(null);
     const projectsRef = useRef(null);
     const homeRef = useRef(null);
-    const handleNavClicks = (e) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (window.location.pathname === "/resume") {
+            resumeRef.current.classList.add("active");
+        } else if (window.location.pathname === "/projects") {
+            projectsRef.current.classList.add("active");
+        } else {
+            homeRef.current.classList.add("active");
+        }
+    }, []);
+
+    const setClassName = (e, name, className) => {
         e.preventDefault();
-        resumeRef.current.classList.remove('active');
-        projectsRef.current.classList.remove('active');
-        homeRef.current.classList.remove('active');
-        e.target.classList.add('active');
+        if (name === "resume") {
+            projectsRef.current.classList.remove("active");
+            homeRef.current.classList.remove("active");
+            resumeRef.current.classList.add(className);
+            navigate("/resume");
+        } else if (name === "projects") {
+            resumeRef.current.classList.remove("active");
+            homeRef.current.classList.remove("active");
+            projectsRef.current.classList.add(className);
+        } else if (name === "home") {
+            resumeRef.current.classList.remove("active");
+            projectsRef.current.classList.remove("active");
+            homeRef.current.classList.add(className);
+        }
     }
 
     return (
         <>
             <div className="navbar">
                 <div className="nav-items">
-                    <li ref={homeRef} className="active"><a href="/">&lt;dev-shivpujan /&gt;</a></li>
-                    <li ref={resumeRef} onClick={(e)=>handleNavClicks(e)}><a href="/">Resume</a></li>
-                    <li ref={projectsRef}><a href="/">Projects</a></li>
+                    <li ref={homeRef} className="active"
+                        onClick={(e) => setClassName(e, "home", "active")}
+                    ><a href="/">&lt;dev-shivpujan /&gt;</a></li>
+                    <li ref={resumeRef}
+                        onClick={(e) => setClassName(e, "resume", "active")}
+                    ><a href="/">Resume</a></li>
+                    <li ref={projectsRef}
+                        onClick={(e) => setClassName(e, "projects", "active")}
+                    ><a href="/">Projects</a></li>
                     {/*<li><a href="/about"></a></li>*/}
                 </div>
             </div>
-            <div className="container">
-                <div className="intro-text">
-                    <li>hi!, I'm</li>
-                    <li>Shivpujan yadav</li>
-                    <li>a Software Developer</li>
-                </div>
-                <div className="fade-img">
-                    <img src={SomeImage} alt="profile"/>
-                </div>
-            </div>
+            <Home />
+
             <div className="bottom">
 
                 <a href="https://www.linkedin.com/in/shivpujan-yadav-b98485142/"
