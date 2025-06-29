a30593236ac0ce24bc530746a602e535a73aefa6
Shivpujan yadav <31546372+shivpujan12@users.noreply.github.com>
2025-06-29 17:00:02 +0530

    Added header icon, update the header and the header title

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import './App.css';
-import Resume from "./components/Resume";
-import Projects from "./components/Projects";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import NoPage from "./components/NoPage";
 import Header from "./components/Header";
-import Home from "./components/Home";
-import WebProject from "./components/WebProject";
-import AndroidProject from "./components/AndroidProject";
-import WorkEx from "./components/WorkEx";
 
 function App() {
   return (
-      <div className="parent">
-          <BrowserRouter>
-              <Header/>
-              <Routes>
-                  <Route path="/" element={<Home/>}/>
-                  <Route path="/home" element={<Home />}/>
-                  <Route path="/resume" element={<Resume/>}/>
-                  <Route path="/projects" element={<Projects/>}>
-                      <Route path="" element={<WebProject/>}/>
-                      <Route path="web" element={<WebProject/>}/>
-                      <Route path="android" element={<AndroidProject/>}/>
-                      <Route path="upcoming" element={<WorkEx/>}/>
-                  </Route>
-                  <Route path="*" element={<NoPage/>}/>
-              </Routes>
-          </BrowserRouter>
+      <div>
+          <Header />
       </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,79 +1,20 @@
-import {useEffect, useRef} from "react";
-import {useNavigate} from "react-router-dom";
 import '../styles/Header.scoped.css';
 
-export default function() {
-
-    const resumeRef = useRef(null);
-    const projectsRef = useRef(null);
-    const homeRef = useRef(null);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (window.location.pathname === "/resume") {
-            projectsRef.current.classList.remove("active");
-            homeRef.current.classList.remove("active");
-            resumeRef.current.classList.add("active");
-            navigate("/resume");
-        } else if (window.location.pathname === "/projects") {
-            resumeRef.current.classList.remove("active");
-            homeRef.current.classList.remove("active");
-            projectsRef.current.classList.add("active");
-            navigate("/projects")
-        } else {
-            resumeRef.current.classList.remove("active");
-            projectsRef.current.classList.remove("active");
-            homeRef.current.classList.add("active");
-            navigate("/home")
-        }
-    }, []);
-
-    const setClassName = (e, name) => {
-        e.preventDefault();
-        if (name === "resume") {
-            projectsRef.current.classList.remove("active");
-            homeRef.current.classList.remove("active");
-            resumeRef.current.classList.add("active");
-            navigate("/resume");
-        } else if (name === "projects") {
-            resumeRef.current.classList.remove("active");
-            homeRef.current.classList.remove("active");
-            projectsRef.current.classList.add("active");
-            navigate("/projects")
-        } else if (name === "home") {
-            resumeRef.current.classList.remove("active");
-            projectsRef.current.classList.remove("active");
-            homeRef.current.classList.add("active");
-            navigate("/home")
-        }
-    }
+const Header = ()=> {
 
     return (
-        <div className="navbar">
-            <div className="nav-items">
-                <li ref={homeRef} className="active"
-                    onClick={(e) => setClassName(e, "home")}
-                ><a href="/">&lt;dev-shivpujan /&gt;</a></li>
-                <li ref={resumeRef}
-                    onClick={(e) => setClassName(e, "resume")}
-                ><a href="/">Resume</a></li>
-                <li ref={projectsRef}
-                    onClick={(e) => setClassName(e, "projects")}
-                ><a href="/">Projects</a></li>
-                {/*<li><a href="/about"></a></li>*/}
+        <nav className={'navbar inter-regular'}>
+            <div className={'nav-logo inter-bold'}>{`<CodeByShiv/>`}</div>
+            <div className={'nav-items'}>
+                <li className={'active'}>Home</li>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Skills</li>
+                <li>Contact</li>
             </div>
-            <div className="mobile-menu">
-                <li ref={resumeRef}
-                    onClick={(e) => setClassName(e, "resume")}
-                ><a href="/">Resume</a></li>
-                <li ref={homeRef} className="active"
-                    onClick={(e) => setClassName(e, "home")}
-                ><a href="/">&lt;dev-shivpujan /&gt;</a></li>
-                <li ref={projectsRef}
-                    onClick={(e) => setClassName(e, "projects")}
-                ><a href="/">Projects</a></li>
-            </div>
-        </div>
-
+        </nav>
     )
 }
+
+export default Header;
