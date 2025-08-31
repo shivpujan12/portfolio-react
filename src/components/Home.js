de7231495f2d536729404030d666d2abead7ab87
Shivpujan yadav <31546372+shivpujan12@users.noreply.github.com>
2025-09-01 00:44:14 +0530

    updated the white background to different color background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import '../styles/Bottom.scoped.css';
 import '../styles/Home.scoped.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faToolbox} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home(){
     return (
@@ -23,7 +25,9 @@ export default function Home(){
 
 const InfoBar = () => {
     return (
-        <div className="info-bar"></div>
+        <div className="info-bar">
+            <span> Skill Inventory  <FontAwesomeIcon icon={faToolbox}/></span>
+        </div>
     )
 }
 
